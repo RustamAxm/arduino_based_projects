bb96cbe6408f3b66971fb3eb08660d32731e5f16
Rustam Akhmadullin <rustamaxm@gmail.com>
2023-05-01 22:01:04 +0300

    fix water pump

---
+++ water_pump/water_pump.ino
@@ -0,0 +1,46 @@
+#include <Wire.h> 
+#include <iarduino_RTC.h> 
+iarduino_RTC watch(RTC_DS1302, 2, 3, 4);
+int T_pump = 30000;
+
+void setup() {
+    // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(8, OUTPUT);
+  watch.begin();
+  //  watch.settime(0,48,21, 1, 1,23,1); 
+  Serial.begin(9600);
+  Serial.println(watch.gettime("d-m-Y, H:i:s, D"));
+}
+void Pump_ON() {
+  digitalWrite(13, HIGH);
+  digitalWrite(8, LOW);
+  analogWrite(11, 255);
+  Serial.println("PUMP_ON");
+  delay(T_pump);
+}
+
+void Pump_OFF() {
+  digitalWrite(13, HIGH);
+  digitalWrite(8, LOW);
+  analogWrite(11, 0);
+  Serial.println("PUMP_OFF");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (millis()%1000==0) {
+    watch.gettime();
+    Serial.print(watch.Hours);   Serial.print(":");  // Вывести часы.
+    Serial.print(watch.minutes); Serial.print(":");  // Вывести минуты.
+    Serial.print(watch.seconds); Serial.println(""); // Вывести секунды.
+    
+    if ((watch.day%2==1)and(watch.Hours==18) and (watch.minutes == 30) and (watch.seconds == 0)) {
+      Pump_ON();
+    } else {
+      Pump_OFF();
+    }
+    delay(1);
+  }
+}
