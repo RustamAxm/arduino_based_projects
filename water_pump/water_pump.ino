d1b0352d4f048426dde6ce1b8d637ae072876894
Rustam Akhmadullin <rustamaxm@gmail.com>
2023-05-05 21:40:20 +0300

    fix to rb trip

--- water_pump/water_pump.ino
+++ water_pump/water_pump.ino
@@ -1,24 +1,25 @@
 #include <Wire.h> 
 #include <iarduino_RTC.h> 
 iarduino_RTC watch(RTC_DS1302, 2, 3, 4);
-int T_pump = 30000;
-
+int T_pump = 40;
+int T_tmp = 0;
+String current_time = "";
 void setup() {
     // put your setup code here, to run once:
   pinMode(13, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(8, OUTPUT);
   watch.begin();
-  //  watch.settime(0,48,21, 1, 1,23,1); 
+  //  watch.settime(0,19,21, 5, 5,23,5); 
   Serial.begin(9600);
-  Serial.println(watch.gettime("d-m-Y, H:i:s, D"));
+  current_time = watch.gettime("H:i:s");
+  Serial.println(current_time);
 }
 void Pump_ON() {
   digitalWrite(13, HIGH);
   digitalWrite(8, LOW);
   analogWrite(11, 255);
   Serial.println("PUMP_ON");
-  delay(T_pump);
 }
 
 void Pump_OFF() {
@@ -30,17 +31,28 @@ void Pump_OFF() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // if (Serial.available()) {
+  //   T_tmp = Serial.parseInt();
+  //   T_pump = T_tmp;
+  //   Serial.print("Setted T_pump = ");
+  //   Serial.println(T_pump);
+  // }
+
   if (millis()%1000==0) {
-    watch.gettime();
-    Serial.print(watch.Hours);   Serial.print(":");  // Вывести часы.
-    Serial.print(watch.minutes); Serial.print(":");  // Вывести минуты.
-    Serial.print(watch.seconds); Serial.println(""); // Вывести секунды.
+    current_time = watch.gettime("H:i:s");
+    Serial.println(current_time);
+    Serial.println(T_pump);
     
-    if ((watch.day%2==1)and(watch.Hours==18) and (watch.minutes == 30) and (watch.seconds == 0)) {
-      Pump_ON();
+    if ((current_time == "21:35:00") and (watch.day % 2)) {
+      while (watch.seconds < T_pump) {
+        Pump_ON();
+        current_time = watch.gettime("H:i:s");
+        delay(500);
+      }
     } else {
       Pump_OFF();
     }
-    delay(1);
+    delay(10);
   }
 }
+
